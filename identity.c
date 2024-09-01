551b7aea3592636d220e0f964d45ca823e1cb4cd
joynto7 <joyntoghosh7@gmail.com>
2024-09-01 16:41:50 +0600

    Area of a Circle

--- identity.c
+++ identity.c
@@ -1,4 +1,7 @@
-#include <stdio.h>
+
+// Area of Rectangel
+ 
+ #include <stdio.h>
 void main(){
     float a,b,area;
     printf("Enter Height:");
@@ -6,6 +9,20 @@ void main(){
     printf("Enter Wdith:");
     scanf("%f",&b);
     area=a*b;
-    printf("The area of Rectangular is:%.2f",area);
+    printf("The area of Rectangle is:%.2f",area);
+
+}
+
+
+// Area of a circle 
+
+#include<stdio.h>
+void main(){
+    float r,pi,area;
+    pi=3.1416;
+    printf("Enter the radius");
+    scanf("%f",&r);
+    area=pi*r*r;
+    printf("Area of Circle:");
 
 }
